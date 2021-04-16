87d358488cf4df1efbcbc0ea0cb757bbec641009
Ceciliebui <cblp26882@edu.ucl.dk>
2021-04-16 14:48:43 +0200

    Kontaktformular

---
+++ scripts/kontaktformular.js
@@ -0,0 +1,17 @@
+console.log ("javascript is running");
+
+document.getElementById("email")
+document.getElementById("emne")
+document.getElementById("beskrivelse")
+document.getElementById("sendknap")addEventListener("click", validerEmail);
+
+let mail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+if (document.getElementById("email") = let mail) {
+  console.log ("True");
+}
+
+
+function validerEmail () {
+
+}
