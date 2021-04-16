e9226c0627b067e333aa7d6fccffe9770ae4f7f0
Mads R. A <madsmeda@gmail.com>
2021-04-16 09:21:03 +0200

    test

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ console.log('script.js is running...');
 
 
 // Sticky header - Mads
-const header = document.getElementById('header');
-const sticky = header.offsetTop;
+// const header = document.getElementById('header');
+// const sticky = header.offsetTop;
 
-function addStickyClassToHeader() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-}
+// function addStickyClassToHeader() {
+//     if (window.pageYOffset > sticky) {
+//         header.classList.add("sticky");
+//     } else {
+//         header.classList.remove("sticky");
+//     }
+// }
 
-window.addEventListener('scroll', addStickyClassToHeader);
+// window.addEventListener('scroll', addStickyClassToHeader);
 
