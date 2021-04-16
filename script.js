1822cc7cba670f014ebde668549a3205a091c612
Mads R. A <madsmeda@gmail.com>
2021-04-16 08:34:56 +0200

    sticky header

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('script.js is running...');
+
+
+// Sticky header - Mads
+const header = document.getElementById('header');
+const sticky = header.offsetTop;
+
+
+function addStickyClassToHeader() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+window.addEventListener('scroll', addStickyClassToHeader);
+
