4bc42b12416f8ba2385821f51dc2b83525909360
Ceciliebui <cblp26882@edu.ucl.dk>
2021-04-18 14:12:43 +0200

    Kontaktformular med javascript

--- scripts/kontaktformular.js
+++ scripts/kontaktformular.js
@@ -1,17 +1,50 @@
 console.log ("javascript is running");
 
-document.getElementById("email")
-document.getElementById("emne")
-document.getElementById("beskrivelse")
-document.getElementById("sendknap")addEventListener("click", validerEmail);
+let mail = /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+let email = document.getElementById("email");
+console.log (email);
+let emne = document.getElementById("emne");
+let beskrivelse = document.getElementById("beskrivelse");
+let sendknap = document.getElementById("sendknap");
 
-let mail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+sendknap.addEventListener("click", sendInfo);
 
-if (document.getElementById("email") = let mail) {
-  console.log ("True");
-}
 
 
 function validerEmail () {
+  if (email.value === "") {
+    return false
+  }
+  else {
+    return true
+  }
+}
 
+function validerEmne () {
+  if (emne.value === "") {
+    console.log ("Udfyld emnefelt")
+    return false
+  }
+  else {
+    return true
+  }
+}
+
+function validerBeskrivelse () {
+  if (beskrivelse.value === "") {
+    return false
+  }
+  else {
+    return true
+  }
+}
+
+function sendInfo (e) {
+e.preventDefault()
+  if (validerEmail() && validerEmne() && validerBeskrivelse()) {
+    console.log ("Tak for info")
+  }
+  else {
+    console.log ("Udfyld alle felter")
+  }
 }
