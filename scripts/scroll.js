fcffb58b208fe01cc83a1531e924c8bea30cb7d9
Kotharion <46055261+Kotharion@users.noreply.github.com>
2021-04-19 13:00:54 +0200

    Create scroll.js
    Scrollbar

---
+++ scripts/scroll.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {omegaFunktion()};
+
+function omegaFunktion() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("minBar").style.width = scrolled + "%";
+}
