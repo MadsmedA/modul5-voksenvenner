abdc5f328053d299cbebf0667f50d57f9060c130
Frisdahl <ajfr26311@edu.ucl.dk>
2021-04-19 13:00:11 +0200

    Script tilføjet til nyhedsbrev for footer
    tilføjet på forside og kontakt. (footer)

--- scripts/nyhedsbrev.js
+++ scripts/nyhedsbrev.js
@@ -1,4 +1,4 @@
-var validateEmail = function(elementValue) {
+var validateEmail = function nyhedsbrev(elementValue) {
     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     return emailPattern.test(elementValue);
 }
@@ -14,22 +14,5 @@ $('#email').keyup(function() {
     } else {
         $(this).css('color', '#2bb673');
  $('.addbut1').prop('disabled', false);
-    }
-
-
-
-    function validatorBtn() {
-      var green = $(this).css(email);
-      var red = $(this).css(email);
-
-      console.log(green);
-
-
-      if (green == 'color', 'green') {
-        alert('Mange tak for din tilmeldelse')
-      } else (var == 'color', 'red')
-        alert('den indtastede mail er ikke korrekt')
-
-
     }
 });
